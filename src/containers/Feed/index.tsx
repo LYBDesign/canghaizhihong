0f0c1a077427ebcc01cc5449f947f9b21296c99f
Wu Bo <53469981+WuboDev@users.noreply.github.com>
2023-04-17 20:37:19 +0800

    Update index.tsx

--- src/containers/Feed/index.tsx
+++ src/containers/Feed/index.tsx
@@ -17,7 +17,7 @@ const Feed: React.FC<Props> = ({ tags, posts }) => {
   const [q, setQ] = useState('')
 
   return (
-    <div className="block md:grid grid-cols-12 gap-6">
+    <div className="col-span-12 lg:col-span-7">
       <Lists.TagList className="hidden lg:block col-span-2" data={tags} />
       <div className="col-span-12 lg:col-span-7">
         <Cards.MobileProfileCard />
