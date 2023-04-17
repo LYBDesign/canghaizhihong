33d169abc08270d7fc0b1271307f2bb3c21b9666
Wu Bo <53469981+WuboDev@users.noreply.github.com>
2023-04-17 20:26:18 +0800

    Update MobileProfileCard.tsx

--- src/containers/Feed/components/cards/MobileProfileCard.tsx
+++ src/containers/Feed/components/cards/MobileProfileCard.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const MobileProfileCard: React.FC<Props> = () => {
   return (
-    <div className="block lg:hidden">
+    <div className="block lg:hidden" style={{display: 'none'}}>
       <div className="p-1 mb-3 dark:text-white">💻 Profile</div>
       <div className="p-2 rounded-2xl bg-white dark:bg-zinc-700 mb-4">
         <div className="flex items-center gap-2">
